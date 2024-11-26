9f1cc26a9154e4cab6a9f5f3fe0ebb01a8980cf0
Usman Saif <usman.saif22@gmail.com>
2024-11-27 02:26:35 +0500

    auto create CNAME file after cleanup

--- scripts/clean.js
+++ scripts/clean.js
@@ -4,4 +4,5 @@ const upath = require('upath');
 const destPath = upath.resolve(upath.dirname(__filename), '../docs');
 
 sh.rm('-rf', `${destPath}/*`)
-
+// Create CNAME file with content "usmansaif.work"
+sh.ShellString('usmansaif.work').to(`${destPath}/CNAME`);
